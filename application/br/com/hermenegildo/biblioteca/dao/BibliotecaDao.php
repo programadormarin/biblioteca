c31bafb1d6034b1dd13efbda73e3adf5d0201135
Hermengildo Marin Júnior <programador.marin@gmail.com>
2011-03-29 20:38:19 -0300

    Agora sim

---
+++ application/br/com/hermenegildo/biblioteca/dao/BibliotecaDao.php
@@ -0,0 +1,68 @@
+<?php
+namespace br\com\hermenegildo\biblioteca\dao {
+	use br\com\hermenegildo\biblioteca\entities\Biblioteca;
+
+	use br\com\hermenegildo\biblioteca\util\ConexaoMysql;
+
+	class BibliotecaDao {
+	
+		public function getById($id) {
+			$pdo = ConexaoMysql::getInstance();
+			$sql = 'SELECT * FROM autor WHERE id = ?';
+			$stm = $pdo->prepare($sql);
+			$stm->execute(array($id));
+			if ($obj = $stm->fetchObject()) {
+				$biblioteca = new Biblioteca();
+				$autor->setId($obj->id);
+				$autor->setNome($obj->nome);
+				return $usuario;
+			} else {
+				throw new PDOException('Não foi encontrado autor com a id ' . $id);
+			}
+		}
+		
+		public function listAll() {
+			$pdo = ConexaoMysql::getInstance();
+			$sql = 'SELECT * FROM autor ORDER BY `id`';
+			$stm = $pdo->prepare($sql);
+			$stm->execute(array($id));
+			$autores = new SplFixedArray($stm->rowCount());
+			while ($obj = $stm->fetchObject()) {
+				$autor = new Autor();
+				$autor->setId($obj->id);
+				$autor->setNome($obj->nome);
+				$autores[] = $autor;
+			}
+			return $autores;
+		}
+		
+		public function save(Autor $autor) {
+			$pdo = ConexaoMysql::getInstance();
+			
+			if ($autor->getId()) {
+				$stm = $pdo->prepare('UPDATE autor SET nome = ? WHERE id = ?');
+				$stm->bindValue(1, $autor->getNome());
+				$stm->bindValue(2, $autor->getId());
+				$stm->execute(array($autor->getNome(), $autor->getId()));
+			} else {
+				$stm = $pdo->prepare('INSERT INTO autor (nome) VALUES (?) LIMIT 1');
+				$stm->bindValue(1, $autor->getNome());
+				$stm->execute(array($autor->getNome()));
+				
+				$autor->setId($pdo->lastInsertId());				
+			}
+		}
+		
+		public function remove (Autor $autor) {
+			$pdo = ConexaoMysql::getInstance();
+			
+			if ($autor->getId()) {
+				$stm = $pdo->prepare('DELETE FROM autor WHERE id = ?');
+				$stm->bindValue(1, $autor->getId());
+				$stm->execute(array($autor->getId()));
+			} else {
+				throw new PDOException('Parâmetro id é obrigatório.');		
+			}
+		}
+	}
+}
--- application/br/com/hermenegildo/biblioteca/dao/UsuarioDao.php
+++ application/br/com/hermenegildo/biblioteca/dao/UsuarioDao.php
@@ -1,18 +1,59 @@
 <?php
-use br\com\hermenegildo\biblioteca\entities\Emprestimo;
-use br\com\hermenegildo\biblioteca\entities\Usuario;
-class UsuarioDao extends \OutletDaoSupport
-{
-	public function save(\br\com\hermenegildo\biblioteca\entities\Usuario $usuario)
-	{
-		$this->getOutlet()->save($usuario);
-	}
-	
-	public function getById($id)
-	{
-		$emprestimo = new Emprestimo();
-		$emprestimo->getData()->format('Y-m-d H:i:s');
+namespace br\com\hermenegildo\biblioteca\dao {
+	use br\com\hermenegildo\biblioteca\util\ConexaoMysql;
+	use br\com\hermenegildo\biblioteca\entities\Usuario;
+	class UsuarioDao {
+		public function getById($id) {
+			$pdo = ConexaoMysql::getInstance();
+			$stm = $pdo->prepare('SELECT * FROM autor WHERE id = ?');
+			$stm->execute(array($id));
+			if ($obj = $stm->fetchObject()) {
+				$usuario = new Usuario();
+				$usuario->setId($obj->id);
+				$usuario->setCpf($obj->cpf);
+				$usuario->setNome($obj->nome);
+				return $usuario;
+			} else {
+				throw new PDOException('Não foi encontrado usuário');
+			}
+		}
 		
-		return $this->getOutlet()->load('Usuario', $id);
+		public function listAll() {
+			$pdo = ConexaoMysql::getInstance();
+			$stm = $pdo->prepare('SELECT * FROM autor ORDER BY id');
+			$stm->execute(array($id));
+			$usuarios = new SplFixedArray($stm->rowCount());
+			while ($obj = $stm->fetchObject()) {
+				$usuario = new Usuario();
+				$usuario->setId($obj->id);
+				$usuario->setCpf($obj->cpf);
+				$usuario->setNome($obj->nome);
+				$usuarios[] = $usuario;
+			}
+			return $usuarios;
+		}
+		
+		public function save(Usuario $usuario) {
+			$pdo = ConexaoMysql::getInstance();
+			if ($usuario->getId()) {
+				$smt = $pdo->prepare('UPDATE FROM usuario set cpf = ?, nome = ? WHERE id = ? LIMIT 1');
+				$smt->bindValue(1, $usuario->getCpf());
+				$smt->bindValue(2, $usuario->getNome());
+				$smt->bindValue(3, $usuario->getId());
+				$smt->execute();
+			} else {
+				$smt = $pdo->prepare('INSERT INTO usuario (nome, cpf) VALUES(?, ?)');
+				$smt->bindValue(1, $usuario->getCpf());
+				$smt->bindValue(2, $usuario->getNome());
+				$smt->execute();
+			}
+		}
+		
+		public function remove (Usuario $usuario) {
+			$pdo = ConexaoMysql::getInstance();
+			$stm = $pdo->prepare('DELETE FROM usuario WHERE id = ?');
+			$stm->bindValue(1, $usuario->getId());
+			$stm->execute();
+		}
 	}
 }
