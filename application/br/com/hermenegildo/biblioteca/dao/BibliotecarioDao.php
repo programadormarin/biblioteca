5eaf9fd056a7967c97be5369e0df20fd36073e53
Hermengildo Marin Júnior <programador.marin@gmail.com>
2011-03-31 19:17:31 -0300

    Todo dao ok

--- application/br/com/hermenegildo/biblioteca/dao/BibliotecarioDao.php
+++ application/br/com/hermenegildo/biblioteca/dao/BibliotecarioDao.php
@@ -1,48 +1,53 @@
 <?php
 namespace br\com\hermenegildo\biblioteca\dao {
+	use br\com\hermenegildo\biblioteca\entities\Bibliotecario;
+
 	use br\com\hermenegildo\biblioteca\util\ConexaoMysql;
 
 	class AutorDao {
 	
 		public function getById($id) {
 			$pdo = ConexaoMysql::getInstance();
-			$sql = 'SELECT * FROM autor WHERE id = ?';
+			$sql = 'SELECT * FROM bibliotecario WHERE id = ?';
 			$stm = $pdo->prepare($sql);
 			$stm->execute(array($id));
 			if ($obj = $stm->fetchObject()) {
-				$autor = new Autor();
-				$autor->setId($obj->id);
-				$autor->setNome($obj->nome);
-				return $autor;
+				$bibliotecario = new Bibliotecario();
+				$bibliotecario->setId($obj->id);
+				$bibliotecario->setNome($obj->nome);
+				$bibliotecario->setLogin($obj->login);
+				return $bibliotecario;
 			} else {
-				throw new PDOException('Não foi encontrado autor com a id ' . $id);
+				throw new PDOException('Não foi encontrado bibliotecario com a id ' . $id);
 			}
 		}
 		
 		public function listAll() {
 			$pdo = ConexaoMysql::getInstance();
-			$stm = $pdo->prepare('SELECT * FROM autor ORDER BY `id`');
+			$stm = $pdo->prepare('SELECT * FROM bibliotecario ORDER BY `id`');
 			$stm->execute();
-			$autores = new SplFixedArray($stm->rowCount());
+			$bibliotecarios = new SplFixedArray($stm->rowCount());
 			while ($obj = $stm->fetchObject()) {
-				$autor = new Autor();
-				$autor->setId($obj->id);
-				$autor->setNome($obj->nome);
-				$autores[] = $autor;
+				$bibliotecario = new Bibliotecario();
+				$bibliotecario->setId($obj->id);
+				$bibliotecario->setNome($obj->nome);
+				$bibliotecario->setLogin($obj->login);
+				$bibliotecarios[] = $bibliotecario;
 			}
-			return $autores;
+			return $bibliotecarios;
 		}
 		
-		public function save(Autor $autor) {
+		public function save(Bibliotecario $bibliotecario) {
 			$pdo = ConexaoMysql::getInstance();
 			
 			if ($autor->getId()) {
-				$stm = $pdo->prepare('UPDATE autor SET nome = ? WHERE id = ?');
-				$stm->bindValue(1, $autor->getNome());
-				$stm->bindValue(2, $autor->getId());
+				$stm = $pdo->prepare('UPDATE bibliotecario SET nome = ?, login = ? WHERE id = ?');
+				$stm->bindValue(1, $bibliotecario->getNome());
+				$stm->bindValue(2, $bibliotecario->getLogin());
+				$stm->bindValue(3, $bibliotecario->getId());
 				$stm->execute();
 			} else {
-				$stm = $pdo->prepare('INSERT INTO autor (nome) VALUES (?) LIMIT 1');
+				$stm = $pdo->prepare('INSERT INTO bibliotecario (nome) VALUES (?) LIMIT 1');
 				$stm->bindValue(1, $autor->getNome());
 				$stm->execute();
 				
@@ -50,12 +55,12 @@ namespace br\com\hermenegildo\biblioteca\dao {
 			}
 		}
 		
-		public function remove (Autor $autor) {
+		public function remove (Bibliotecario $bibliotecario) {
 			$pdo = ConexaoMysql::getInstance();
 			
-			if ($autor->getId()) {
-				$stm = $pdo->prepare('DELETE FROM autor WHERE id = ?');
-				$stm->bindValue(1, $autor->getId());
+			if ($bibliotecario->getId()) {
+				$stm = $pdo->prepare('DELETE FROM bibliotecario WHERE id = ?');
+				$stm->bindValue(1, $bibliotecario->getId());
 				$stm->execute();
 			} else {
 				throw new PDOException('Parâmetro id é obrigatório.');		
